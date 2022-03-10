f28e21fd46fad5f06d233539a550b9d33cbb48e2
Ilyos Nishanov <n.ilyosjon@gmail.com>
2022-03-10 15:48:45 +0500

    writing .txt tidy data

--- run_analysis.R
+++ run_analysis.R
@@ -151,5 +151,6 @@ TIDY <- dt[, list(count = .N, average = mean(value)), by = key(dt)]
 key(TIDY)
 
 ################# AND SAVE THE THING
-f <- file.path(".", "TIDY_HumanActivity.csv")
-write.csv(TIDY, f, quote = FALSE, row.names = FALSE)
+f <- file.path(".", "tidy_data.txt")
+write.table(TIDY, f, quote = FALSE, sep = "\t", row.names = FALSE)
+f <- file.path(".", "tidy_data.csv")
